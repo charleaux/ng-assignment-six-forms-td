a8af7db95cc86fa9be4ebff2645bfd9a63f49ff2
Charles Torry <ctorry@gmail.com>
2019-03-10 21:06:45 -0400

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng-assignment-six-forms-td';
+  @ViewChild('form') signupForm: NgForm;
+  application = {
+    email: '',
+    subscription: '',
+    password: ''
+  };
+  submitted = false;
+  defaultSubscription = 'advanced';
+
+  onSubmit() {
+    this.application.email = this.signupForm.value.email;
+    this.application.subscription = this.signupForm.value.subscription;
+    this.application.password = this.signupForm.value.password;
+    this.submitted = true;
+    this.signupForm.reset({ subscription: this.defaultSubscription });
+  }
+  resetSubmisson() {
+    this.submitted = false;
+    this.application = {
+      email: '',
+      subscription: '',
+      password: ''
+    };
+  }
 }
